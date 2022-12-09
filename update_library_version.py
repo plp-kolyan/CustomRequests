69b6fcc7ccc3ef9eaecf34820beaea8abfbb85d3
DarkRastafar <mvmsurgutmebel@gmail.com>
2022-12-09 16:20:38 +0500

    "проапдейтил скрипт по автообновлению версии пакета"

--- update_library_version.py
+++ update_library_version.py
@@ -11,8 +11,9 @@ def up_version(version_list, index=None):
         return '.'.join(version_list)
     else:
         last_chr = int(version_list[last_index])
-        if last_chr > 9:
-            version_list[last_index] = str(last_chr // 10)
+        last_chr_10 = last_chr / 10
+        if last_chr_10 > 9.9:
+            version_list[last_index] = str(0)
             last_index -= 1
             return up_version(version_list, last_index)
         return '.'.join(version_list)
@@ -22,11 +23,11 @@ def update_version(path_to_file="pyproject.toml"):
     import toml
     data = {}
 
-    with open(path_to_file, "r") as file:
+    with open(path_to_file, "r", encoding="UTF-8") as file:
         data = toml.load(file)
         version_list = data['project']['version'].split('.')
         data['project']['version'] = up_version(version_list)
-    with open(path_to_file, "w") as file:
+    with open(path_to_file, "w", encoding="UTF-8") as file:
         toml.dump(data, file).encode('utf-8')
 
 
